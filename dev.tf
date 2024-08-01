b39f4a445aaa7c320dbb8305fb583143cd257bf6
ramchandra007 <168650437+ramchandra007@users.noreply.github.com>
2024-08-01 14:51:31 +0530

    dev.tf

--- dev.tf
+++ dev.tf
@@ -1,33 +1,39 @@
-
 provider "aws" {
   region     = "ap-northeast-3"
 
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
+resource "aws_instance" "nani" {
+  ami                    = "ami-0db6b6e701fbc0603"
+  instance_type          = "t2.micro"
+  key_name = "kins"
+  vpc_security_group_ids = [ "sg-08c4a01c6da41754f" ]
+  
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get upgrade -y",
+      "sudo apt install python3-pip -y",
+      "sudo apt install python3-virtualenv -y",
+      "sudo apt install python3-venv -y",
+      "/usr/bin/python3 -m venv demo",
+      "source /home/ubuntu/demo/bin/activate",
+      "https://github.com/ramchandra007/Django-WebApp.git",
+      "cd Django-WebApp",
+      "/home/ubuntu/demo/bin/pip install django",
+      "/home/ubuntu/demo/bin/pip install django-crispy-forms",
+      "/home/ubuntu/demo/bin/pip install pillow",
+      "/home/ubuntu/demo/bin/python manage.py makemigrations",
+      "/home/ubuntu/demo/bin/python manage.py migrate",
+      "/home/ubuntu/demo/bin/python manage.py runserver 0.0.0.0:8000",
+    ]
   }
-  required_version = ">= 0.14.9"
-}
 
-resource "aws_instance" "jenkins" {
-  ami           = "ami-0db6b6e701fbc0603"
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y openjdk-11-jdk
-    sudo apt-get install -y maven
-    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io.key
-    echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
-    sudo apt-get update
-    sudo apt-get install -y jenkins
-    sudo systemctl start jenkins
-    sudo systemctl enable jenkins
-    sudo systemctl status jenkins
-  EOF
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("kins.pem")
+    host        = aws_instance.nani.public_ip
+  }
 }
+                                 
